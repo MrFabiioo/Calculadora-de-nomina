ccbe6f06c169ad9f17bccdbbe87922c20e72c62d
MrFabiioo <fabiolin57@gmail.com>
2024-09-11 12:55:42 -0500

    detalles de diseño y tipografia resueltos

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ const numero1= document.getElementById("numero11");
 //console.log(numero1.item)
 
 const fechaa= document.getElementById("fecha1");
-console.log(fechaa.value);
+//console.log(fechaa.value);
 const diaa= document.getElementById("dia1");
 console.log(diaa.value);
+
 
 const boton = document.querySelector("button");
 
 const funcionprueba = ()=>{
     if (seleccion1.value==="6:00 Am" && seleccion2.value==="13:00 Pm" && fechaa.value==="2024-09-11") {
-        valor1.innerText="85.000 peso"
+        valor1.innerText="85.000.02"
         horas1.innerText =7
         numero1.innerText=100
-        diaa.innerText="MIERCOLES"
+        diaa.innerText="Miercoles"
     }else{
         valor1.innerText="0"
         horas1.innerText =0
@@ -34,6 +35,10 @@ const funcionprueba = ()=>{
         console.warn("ERRORRRRRRRRRR")
     }
 }
+
+
+
+
 
 boton.addEventListener("click", funcionprueba)
 
